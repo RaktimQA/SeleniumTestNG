4cacb7a92d74d22c86c81ead2fb2888020eaff1b
root <root@raktim-H81M-S>
2019-03-28 16:56:24 +0530

    revert

--- SeleniumTestNg/src/test/java/test/GithubDemo.java
+++ SeleniumTestNg/src/test/java/test/GithubDemo.java
@@ -1,15 +1,5 @@
 package test;
 
-import com.aventstack.extentreports.SystemAttributeContext;
-import com.aventstack.extentreports.model.SystemAttribute;
-
 public class GithubDemo {
-	
-	
-	private void SystemAttributeContext() {
-		// TODO Auto-generated method stub
-
-	}
-	
 
 }
